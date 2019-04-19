a7ad6c7bae2cae057d2281162e9be02e0998f292
mugunga <mugunga6@gmail.com>
2019-04-19 10:02:44 +0900

    brand

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -139,9 +139,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+        <p class="pull-left">&copy; HireTech <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right"></p>
     </div>
 </footer>
 
