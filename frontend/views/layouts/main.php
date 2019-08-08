ba6d6211fa2999dbdf903632e15e65c496903cfd
mugunga <mugunga6@gmail.com>
2019-08-08 20:54:34 +0900

    Revert: removed home link

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -60,7 +60,12 @@ AppAsset::register($this);
 
     ]);
 
-    $menuItems =[
+    $menuItems = [
+        ['label' => '<i class="fa fa-home "></i>  Home', 'url' => ['/site/index'],
+        'glyphicon'=>'cog',
+        ],
+
+
         ['label' => '<i class="fa fa-edit"></i>  Contact Us', 'url' => ['/site/contact']],
         ['label' => '<i class="fa fa-smile"></i>  Give Us a FeedBack..',
             'url' => 'https://docs.google.com/forms/d/e/1FAIpQLSftczth8fAu4k7Jn2HT1PMDHdNoQLdK1rDZ8bK7SkkqWGakAQ/viewform?usp=sf_link',
