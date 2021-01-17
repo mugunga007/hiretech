7304dfbf221e83be721c6f102d46c1eb873b84e1
mugunga <mugunga6@gmail.com>
2021-01-17 13:47:44 -0500

    homepage text modified

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -31,7 +31,6 @@ use yii\helpers\Url;
                         <div class="row">
                             <div class="col-sm-6">
                                 <a type="button" href="<?=Url::to(['seeker/create'])?>"
-                                   class="btn btn-lg mybtnprimary">Searching for a job
                                     </a>
                             </div>
 
