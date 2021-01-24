e5021d2a4c63d96462fa732abce25de51a15d25d
mugunga <mugunga6@gmail.com>
2021-01-24 08:15:16 -0500

    edited main.php

--- common/config/main.php
+++ common/config/main.php
@@ -17,11 +17,8 @@ return [
         'showScriptName' => false,
         // Disable r= routes
         'enablePrettyUrl' => true,
-        'rules' => array(
-            '<controller:\w+>/<id:\d+>' => '<controller>/view',
-            '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-            '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-        ),
+        'rules' =>
+[]
     ],
 ];
 
