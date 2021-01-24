874c582db3b423ed363b6639a4ee0ae75f7aede1
mugunga <mugunga6@gmail.com>
2021-01-23 19:46:35 -0500

    ProviderController error correcting

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -43,12 +43,19 @@ return [
         ],
 
         'urlManager' => [
+            'class' => 'yii\web\UrlManager',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                array(
+                    '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                    '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                    '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                ),
             ],
         ],
 
     ],
     'params' => $params,
 ];
+
