c25e023dbc4522abccd32ea75d7c2400db18bb53
mugunga <mugunga6@gmail.com>
2021-01-23 19:50:11 -0500

    ProviderController error correcting

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -47,11 +47,6 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                array(
-                    '<controller:\w+>/<id:\d+>' => '<controller>/view',
-                    '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                    '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-                ),
             ],
         ],
 
